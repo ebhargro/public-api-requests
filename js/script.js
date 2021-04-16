85f7ba91f6acf5f38443db6cf78bcf9c55529add
ebhargro <75944377+ebhargro@users.noreply.github.com>
2021-04-15 22:53:33 -0700

    adding js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Treehouse Unit 5 Project Submission
+// Ebony Hargro
+// Aiming for: Meets Expectations
+
+// Setting up global variables 
+
+const headerDiv = document.getElementsByClassName('header-text-container')
+
+//Get users from Random User Generator API
+
+
+//Display users from Random User Generator API
+
+//Create modal window
+
+//your Gallery items, but you must use JS to create and append them to the `gallery` div.
